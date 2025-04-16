c484e6854e7f9c69949fff39dbbaa24fed515266
Tata0703 <danqing.zhang.personal@gmail.com>
2025-04-16 11:31:48 +0800

    lats agent indent

--- visual-tree-search-backend/app/api/lwats/agents_async/SearchAgents/lats_agent.py
+++ visual-tree-search-backend/app/api/lwats/agents_async/SearchAgents/lats_agent.py
@@ -22,91 +22,91 @@ class LATSAgent(BaseAgent):
         return best_node
 
     async def lats_search(self, websocket=None):
-            terminal_nodes = []
+        terminal_nodes = []
 
-            for i in range(self.config.iterations):
-                await self.websocket_iteration_start(i, websocket=websocket)
-                
-                print(f"Iteration {i}/{self.config.iterations} ...")
-
-                # Step 1: Node Selection
-                ## TODO: move websocket node selection into node_selection method
-                print(f"{GREEN}Step 1: node selection{RESET}")
-                await self.websocket_step_start(step=1, step_name="node_selection", websocket=websocket)
-                node = await self.node_selection(self.root_node)
-                await self.websocket_node_selection(node, websocket=websocket)
-
-                if node is None:
-                    print("All paths lead to terminal nodes with reward 0. Ending search.")
-                    break
-
-                # Step 2: Node Expansion
-                print(f"{GREEN}Step 2: node expansion{RESET}")
-                await self.websocket_step_start(step=2, step_name="node_expansion", websocket=websocket)
-                await self.node_expansion(node, websocket)
-                if node is None:
-                    # all the nodes are terminal, stop the search
-                    print(f"{RED}All nodes are terminal, stopping search{RESET}")
-                    break
-                tree_data = self._get_tree_data()
-                if websocket:
-                    await self.websocket_tree_update(type="tree_update_node_expansion", websocket=websocket, tree_data=tree_data)
-                else:
-                    print_entire_tree(self.root_node)
-
-
-                # Step 3: Evaluation
-                print(f"{GREEN}Step 3: node chilren evaluation{RESET}")
-                await self.websocket_step_start(step=3, step_name="node_children_evaluation", websocket=websocket)
-                await self.node_children_evaluation(node)
-                tree_data = self._get_tree_data()
-                if websocket:
-                    await self.websocket_tree_update(type="tree_update_node_children_evaluation", websocket=websocket, tree_data=tree_data)
-                else:
-                    print("after evaluation")
-                    print_entire_tree(self.root_node)
-
-
-                # Step 4: Simulation
-                print(f"{GREEN}Step 4: simulation{RESET}")
-                await self.websocket_step_start(step=4, step_name="simulation", websocket=websocket)
-                selected_node = max(node.children, key=lambda child: child.value)
-                await self.websocket_node_selection(selected_node, websocket=websocket, type="node_selected_for_simulation")
-                reward, terminal_node = await self.simulation(selected_node, websocket=websocket)
-                terminal_nodes.append(terminal_node)
-                await self.websocket_simulation_result(reward, terminal_node, websocket=websocket)
-
-                if reward == 1:
-                    await self.websocket_search_complete("success", reward, terminal_node.get_trajectory(), websocket=websocket)
-                    return terminal_node
-
-                # Step 5: Backpropagation
-                print(f"{GREEN}Step 5: backpropagation{RESET}")
-                await self.websocket_step_start(step=5, step_name="backpropagation", websocket=websocket)
-                self.backpropagate(terminal_node, reward)
-                tree_data = self._get_tree_data()
-                if websocket:
-                    await self.websocket_tree_update(type="tree_update_node_backpropagation", websocket=websocket, tree_data=tree_data)
-                else:
-                    print("after backpropagation")
-                    print_entire_tree(self.root_node)
-
-            # Find best node
-            all_nodes_list = collect_all_nodes(self.root_node)
-            all_nodes_list.extend(terminal_nodes)
+        for i in range(self.config.iterations):
+            await self.websocket_iteration_start(i, websocket=websocket)
             
-            ## temp change: if reward is the same, choose the deeper node
-            best_child = max(all_nodes_list, key=lambda x: (x.reward, x.depth))
-            
-            if best_child.value >= 0.75:
-                print("Successful trajectory found")
-                await self.websocket_search_complete("success", best_child.value, best_child.get_trajectory(), websocket=websocket)
+            print(f"Iteration {i}/{self.config.iterations} ...")
+
+            # Step 1: Node Selection
+            ## TODO: move websocket node selection into node_selection method
+            print(f"{GREEN}Step 1: node selection{RESET}")
+            await self.websocket_step_start(step=1, step_name="node_selection", websocket=websocket)
+            node = await self.node_selection(self.root_node)
+            await self.websocket_node_selection(node, websocket=websocket)
+
+            if node is None:
+                print("All paths lead to terminal nodes with reward 0. Ending search.")
+                break
+
+            # Step 2: Node Expansion
+            print(f"{GREEN}Step 2: node expansion{RESET}")
+            await self.websocket_step_start(step=2, step_name="node_expansion", websocket=websocket)
+            await self.node_expansion(node, websocket)
+            if node is None:
+                # all the nodes are terminal, stop the search
+                print(f"{RED}All nodes are terminal, stopping search{RESET}")
+                break
+            tree_data = self._get_tree_data()
+            if websocket:
+                await self.websocket_tree_update(type="tree_update_node_expansion", websocket=websocket, tree_data=tree_data)
             else:
-                print("Unsuccessful trajectory found")
-                await self.websocket_search_complete("partial_success", best_child.value, best_child.get_trajectory(), websocket=websocket)
-            await self.playwright_manager.close()
-                
-            return best_child if best_child is not None else self.root_node
+                print_entire_tree(self.root_node)
+
+
+            # Step 3: Evaluation
+            print(f"{GREEN}Step 3: node chilren evaluation{RESET}")
+            await self.websocket_step_start(step=3, step_name="node_children_evaluation", websocket=websocket)
+            await self.node_children_evaluation(node)
+            tree_data = self._get_tree_data()
+            if websocket:
+                await self.websocket_tree_update(type="tree_update_node_children_evaluation", websocket=websocket, tree_data=tree_data)
+            else:
+                print("after evaluation")
+                print_entire_tree(self.root_node)
+
+
+            # Step 4: Simulation
+            print(f"{GREEN}Step 4: simulation{RESET}")
+            await self.websocket_step_start(step=4, step_name="simulation", websocket=websocket)
+            selected_node = max(node.children, key=lambda child: child.value)
+            await self.websocket_node_selection(selected_node, websocket=websocket, type="node_selected_for_simulation")
+            reward, terminal_node = await self.simulation(selected_node, websocket=websocket)
+            terminal_nodes.append(terminal_node)
+            await self.websocket_simulation_result(reward, terminal_node, websocket=websocket)
+
+            if reward == 1:
+                await self.websocket_search_complete("success", reward, terminal_node.get_trajectory(), websocket=websocket)
+                return terminal_node
+
+            # Step 5: Backpropagation
+            print(f"{GREEN}Step 5: backpropagation{RESET}")
+            await self.websocket_step_start(step=5, step_name="backpropagation", websocket=websocket)
+            self.backpropagate(terminal_node, reward)
+            tree_data = self._get_tree_data()
+            if websocket:
+                await self.websocket_tree_update(type="tree_update_node_backpropagation", websocket=websocket, tree_data=tree_data)
+            else:
+                print("after backpropagation")
+                print_entire_tree(self.root_node)
+
+        # Find best node
+        all_nodes_list = collect_all_nodes(self.root_node)
+        all_nodes_list.extend(terminal_nodes)
+        
+        ## temp change: if reward is the same, choose the deeper node
+        best_child = max(all_nodes_list, key=lambda x: (x.reward, x.depth))
+        
+        if best_child.value >= 0.75:
+            print("Successful trajectory found")
+            await self.websocket_search_complete("success", best_child.value, best_child.get_trajectory(), websocket=websocket)
+        else:
+            print("Unsuccessful trajectory found")
+            await self.websocket_search_complete("partial_success", best_child.value, best_child.get_trajectory(), websocket=websocket)
+        await self.playwright_manager.close()
+            
+        return best_child if best_child is not None else self.root_node
 
     async def node_selection(self, node: LATSNode, websocket=None) -> Optional[LATSNode]:   
         if node.is_terminal:
